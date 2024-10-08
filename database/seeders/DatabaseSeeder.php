b9248f647aa45a19f80d5a8553858961cfe3edbf
wpitombeira <wpitombeira.ipms@gmail.com>
2024-10-08 08:35:21 -0300

    wip

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,21 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\{User, Project};
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
+class DatabaseSeeder extends Seeder {
     /**
      * Seed the application's database.
      */
-    public function run(): void
-    {
-        // User::factory(10)->create();
+    public function run(): void {
+        User::factory()->count(200)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::query()->inRandomOrder()->limit(10)->get()->each(function (User $user) {
+            // $user->projects()->saveMany(Project::factory()->count(5)->make());
+            Project::factory()->create(['created_by' => $user->id]);
+        });
+
     }
 }
