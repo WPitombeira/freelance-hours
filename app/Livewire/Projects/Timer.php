82dfb8a3b9408274332ac15098f32f82377b5cd7
wpitombeira <wpitombeira.ipms@gmail.com>
2024-10-10 21:46:01 -0300

    project timer

---
+++ app/Livewire/Projects/Timer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Livewire\Projects;
+
+use App\Models\Project;
+use Livewire\Component;
+
+class Timer extends Component {
+    public Project $project;
+
+    public function timer() {
+
+    }
+
+    public function render() {
+        $Diff = now()->diff($this->project->ends_at);
+        return view('livewire.projects.timer', [
+            'days' => $Diff->d,
+            'hours' => $Diff->h,
+            'minutes' => $Diff->i,
+            'seconds' => $Diff->s,
+        ]);
+    }
+}
